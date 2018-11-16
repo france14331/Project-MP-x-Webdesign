bd47bf23e290e3e7ba745e6aca4068d25d8245c1
PeetTharathanakul <peet12131424@gmail.com>
2018-11-16 13:52:33 +0700

    dish change

--- js.js
+++ js.js
@@ -12,3 +12,58 @@ food.style.backgroundImage = "url(null)";
 
 
 }
+
+function side1(){
+	food = document.getElementById('box-food1')
+	food.style.backgroundImage = "url('side1.png')";
+
+}
+
+function side2(){
+	food = document.getElementById('box-food1')
+	food.style.backgroundImage = "url('side2.png')";
+
+}
+
+function side3(){
+	food = document.getElementById('box-food1')
+	food.style.backgroundImage = "url('side3.png')";
+
+}
+
+function side4(){
+	food = document.getElementById('box-food1')
+	food.style.backgroundImage = "url('side4.png')";
+
+}
+
+function side5(){
+	food = document.getElementById('box-food1')
+	food.style.backgroundImage = "url('side5.png')";
+
+}
+
+function soup1(){
+	soup = document.getElementById('box-food2')
+	soup.style.backgroundImage = "url('soup1.png')";
+}
+
+function soup2(){
+	soup = document.getElementById('box-food2')
+	soup.style.backgroundImage = "url('soup2.png')";
+}
+
+function soup3(){
+	soup = document.getElementById('box-food2')
+	soup.style.backgroundImage = "url('soup3.png')";
+}
+
+function soup4(){
+	soup = document.getElementById('box-food2')
+	soup.style.backgroundImage = "url('soup4.png')";
+}
+
+function soup5(){
+	soup = document.getElementById('box-food2')
+	soup.style.backgroundImage = "url('soup5.png')";
+}
