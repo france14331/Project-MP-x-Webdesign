8f0cba661c644d5fc95bbcf1941ae97f8bc80db9
PeetTharathanakul <peet12131424@gmail.com>
2018-11-16 15:20:03 +0700

    เพิ่ม ห่อหมก

--- js.js
+++ js.js
@@ -1,46 +1,42 @@
 
 function del1(){
-food = document.getElementById('box-food1')
-food.style.backgroundImage = "url(null)";
-
-
+	food = document.getElementById('box-food1')
+	food.style.backgroundImage = "url(null)";
 }
 
 function del2(){
-food = document.getElementById('box-food2')
-food.style.backgroundImage = "url(null)";
+	food = document.getElementById('box-food2')
+	food.style.backgroundImage = "url(null)";
+}
 
-
+function del3(){
+	food = document.getElementById('etc')
+	food.style.backgroundImage = "url(null)";
 }
 
 function side1(){
 	food = document.getElementById('box-food1')
 	food.style.backgroundImage = "url('side1.png')";
-
 }
 
 function side2(){
 	food = document.getElementById('box-food1')
 	food.style.backgroundImage = "url('side2.png')";
-
 }
 
 function side3(){
 	food = document.getElementById('box-food1')
 	food.style.backgroundImage = "url('side3.png')";
-
 }
 
 function side4(){
 	food = document.getElementById('box-food1')
 	food.style.backgroundImage = "url('side4.png')";
-
 }
 
 function side5(){
 	food = document.getElementById('box-food1')
 	food.style.backgroundImage = "url('side5.png')";
-
 }
 
 function soup1(){
@@ -66,4 +62,9 @@ function soup4(){
 function soup5(){
 	soup = document.getElementById('box-food2')
 	soup.style.backgroundImage = "url('soup5.png')";
+}
+
+function etc(){
+	etc = document.getElementById('etc')
+	etc.style.backgroundImage = "url('etc1.png')";
 }
