ff37f6d21e480042ae29756adc25985624d737e8
france14331 <59070042@it.kmitl.ac.th>
2018-11-16 16:22:19 +0700

    Change size food on plate

--- js.js
+++ js.js
@@ -17,26 +17,31 @@ function del3(){
 function side1(){
 	food = document.getElementById('box-food1')
 	food.style.backgroundImage = "url('side1.png')";
+	food.style.backgroundSize = "100px 100px";
 }
 
 function side2(){
 	food = document.getElementById('box-food1')
 	food.style.backgroundImage = "url('side2.png')";
+	food.style.backgroundSize = "100px 100px";
 }
 
 function side3(){
 	food = document.getElementById('box-food1')
 	food.style.backgroundImage = "url('side3.png')";
+	food.style.backgroundSize = "100px 100px";
 }
 
 function side4(){
 	food = document.getElementById('box-food1')
 	food.style.backgroundImage = "url('side4.png')";
+	food.style.backgroundSize = "100px 100px";
 }
 
 function side5(){
 	food = document.getElementById('box-food1')
 	food.style.backgroundImage = "url('side5.png')";
+	food.style.backgroundSize = "100px 100px";
 }
 
 function soup1(){
@@ -67,4 +72,5 @@ function soup5(){
 function etc(){
 	etc = document.getElementById('etc')
 	etc.style.backgroundImage = "url('etc1.png')";
+	etc.style.backgroundSize = "100px 100px";
 }
