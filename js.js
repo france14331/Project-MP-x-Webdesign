f9e545426227578f2b34564d11b56014986d14e1
PeetTharathanakul <peet12131424@gmail.com>
2018-11-17 15:47:36 +0700

    reset function

--- js.js
+++ js.js
@@ -63,6 +63,62 @@ function del3(){
 	num3 = 0;
 }
 
+function reset(){
+	food1 = document.getElementById('box-food1')
+	food1.style.backgroundImage = 'none';
+	if (num == 1) {
+		change = document.getElementById('side-1')
+		change.src = ('side1.png');
+	}
+	else if (num == 2) {
+		change = document.getElementById('side-2')
+		change.src = ('side2.png');
+	}
+	else if (num == 3) {
+		change = document.getElementById('side-3')
+		change.src = ('side3.png');
+	}
+	else if (num == 4) {
+		change = document.getElementById('side-4')
+		change.src = ('side4.png');
+	}
+	else if (num == 5) {
+		change = document.getElementById('side-5')
+		change.src = ('side5.png');
+	}
+	food2 = document.getElementById('box-food2')
+	food2.style.backgroundImage = 'none';
+	if (num2 == 1) {
+		change = document.getElementById('soup-1')
+		change.src = ('soup1.png');
+	}
+	else if (num2 == 2) {
+		change = document.getElementById('soup-2')
+		change.src = ('soup2.png');
+	}
+	else if (num2 == 3) {
+		change = document.getElementById('soup-3')
+		change.src = ('soup3.png');
+	}
+	else if (num2 == 4) {
+		change = document.getElementById('soup-4')
+		change.src = ('soup4.png');
+	}
+	else if (num2 == 5) {
+		change = document.getElementById('soup-5')
+		change.src = ('soup5.png');
+	}
+	food3 = document.getElementById('etc')
+	food3.style.backgroundImage = 'none';
+	if (num3 == 1) {
+		change = document.getElementById('etcfood')
+		change.src = ('etc1.png');
+	}
+	num = 0;
+	num2 =0;
+	num3 = 0;
+}
+
 function side1(){
 	if (num == 0) {
 	food = document.getElementById('box-food1')
